f4fd2bc3027c704830a5aaf5666adb7b1316aef3
Tomohiro Kumagai <tomohiro@ez-style.jp>
2016-10-08 17:03:12 +0900

    Initial Commit

---
+++ SwiftProposals/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftProposals
+//
+//  Created by Tomohiro Kumagai on 10/8/16.
+//  Copyright © 2016 EasyStyle G.K. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
